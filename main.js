950e217a115b27eb5e9f82f1b8ea71dd84c2b67b
Michael <mrimboim@gmail.com>
2022-02-24 21:20:16 +0000

    url and port now visible when rerun occurs

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const ip = require("ip");
+const app = express();
+const port = 3000; 
+
+app.get("/", (req,res) => {
+    console.log("Here")
+    res.send("hi")
+})
+
+let webLink = 'http://' + ip.address() + ':' + port;
+app.listen(port, () => console.log("listening on port:",port,'\n' + '\u001b[' + 44 + 'm' + webLink + '\u001b[0m'));
+
+
+
