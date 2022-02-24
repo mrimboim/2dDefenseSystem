16b26c055a32d44d135fa6a9eb4635154bc0239d
Michael <mrimboim@gmail.com>
2022-02-24 06:08:00 +0000

    migrated to express

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+const express = require("express")
+const app = express()
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    console.log("testing to see if this gets called everytime page is refreshed")
-    fs.readFile('exhibit/index.html', function (error, data) {
-        if (error) {
-            res.writeHead(404, {"Content-Type": "text/plain"})
-            res.write("Eror: File Not Found")
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-
- 
+app.get("/", (req,res) => {
+    console.log("Here")
+    res.send("hi")
 })
+app.listen(3000)
 
-server.listen(port, function (error) {
-    if (error) {
-        console.log('something went wrong', error)
-    } else {
-        console.log('server is listening on port ' + port)
-    }
-})
+
+
