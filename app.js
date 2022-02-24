c51b1a1d4f3bb6bf6611cb5c0a7a71ec630861d1
Michael <mrimboim@gmail.com>
2022-02-24 04:51:32 +0000

    working on html error notification

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
-const server = http.createServer(function(req, res) {
-    res.write('Hello Node')
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    console.log("testing to see if this gets called everytime page is refreshed")
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Eror: File Not Found')
+
+        } else {
+            res.write(data)
+        }
+    })
+
     res.end()
 })
 
-server.listen(port, function(error) {
+server.listen(port, function (error) {
     if (error) {
         console.log('something went wrong', error)
     } else {
